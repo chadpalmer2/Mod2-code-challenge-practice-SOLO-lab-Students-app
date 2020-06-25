5fc292332b70b79d7d8f70749ee34631eec01088
chad <chad.palmer@yale.edu>
2020-06-25 10:56:35 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,56 @@
 class StudentsController < ApplicationController
 
+  before_action :student_current, only: [:show, :edit, :update]
+  before_action :instructors, only: [:new, :edit]
+
   def index
     @students = Student.all
   end
-  
+
+  def show
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.valid?
+      @student.save
+      redirect_to @student.instructor
+    else
+      flash[:errors] = @student.errors.full_messages
+      redirect_to new_student_path
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @student.assign_attributes(student_params)
+    if @student.valid?
+      @student.save
+      redirect_to @student
+    else
+      flash[:errors] = @student.errors.full_messages
+      redirect_to edit_student_path(@student)
+    end
+  end
+
+  private
+
+  def student_current
+    @student = Student.find(params[:id])
+  end
+
+  def instructors
+    @instructors = Instructor.all
+  end
+
+  def student_params
+    params.require(:student).permit(:name, :major, :age, :instructor_id)
+  end
+
 end
--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,15 @@
 class Instructor < ApplicationRecord
     has_many :students
+
+    def average_student_age
+        if students.count != 0
+            students.sum {|student| student.age} / students.count
+        else
+            0
+        end
+    end
+
+    def students_sort_by_name
+        students.sort_by {|student| student.name}
+    end
 end
